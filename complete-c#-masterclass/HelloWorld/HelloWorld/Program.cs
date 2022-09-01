15d75c4e4ea0541efe97fa437b28fe1f05e2f5d1
haominhe <haominhe@LAPTOP-CSJB3RJP.localdomain>
2022-09-01 15:57:11 -0700

    parse numbers

---
+++ complete-c#-masterclass/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float strFloat = float.Parse(stringForFloat);
+            int strInt = Int32.Parse(stringForInt);
+
+
+            Console.WriteLine("Hi");
+            Console.WriteLine("Int = {0}, Float = {1}", strInt, strFloat);
+            Console.Read();
+        }
+    }
+}
